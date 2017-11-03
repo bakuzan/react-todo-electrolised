290cfb49be1e0d18a7ad6b9bef1c5297372cd9b7
bakuzan <imannoyed4days@hotmail.co.uk>
2017-11-03 13:49:11 +0000

    removed test page.

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import {ConnectedRouter} from 'react-router-redux'
 import {Redirect, Route, Switch} from 'react-router-dom';
 
 import App from 'containers/app'
-import Test from 'views/test'
 import Home from 'views/home/home'
 
 
@@ -13,7 +12,6 @@ const Routes = ({ history }) => (
       <Switch>
         <Redirect exact from="/" to="/home" />
         <Route path="/home" component={Home} />
-        <Route path="/test" component={Test} />
       </Switch>
     </ConnectedRouter>
   </App>
